8ee6ff8960ed212ead4ded9b8ab94c8d8ad36510
ImDamc <damcproseeee@outlook.com>
2025-02-22 19:59:21 -0500

    Update 6872274481.lua

--- Games/6872274481.lua
+++ Games/6872274481.lua
@@ -1,2 +1,3 @@
--- will be put back up when it goes public
-game.Players.LocalPlayer:Kick("will be put back up when it goes public")
+if script_key ~= nil then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2b9c3e0faae2dbdacadd8f55e9c3382a.lua"))()
+end
